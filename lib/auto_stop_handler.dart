e85fc157352471069de72279c1e174100b4cdf47
Quyết Thắng <thang13513110061@gmail.com>
2024-10-04 13:05:55 +0700

    - Bug fixes

--- lib/auto_stop_handler.dart
+++ lib/auto_stop_handler.dart
@@ -5,14 +5,8 @@ import 'background_locator.dart';
 class AutoStopHandler extends WidgetsBindingObserver {
   @override
   Future<void> didChangeAppLifecycleState(AppLifecycleState state) async {
-    switch (state) {
-      case AppLifecycleState.inactive:
-      case AppLifecycleState.paused:
-      case AppLifecycleState.detached:
-        await BackgroundLocator.unRegisterLocationUpdate();
-        break;
-      case AppLifecycleState.resumed:
-        break;
+    if (state != AppLifecycleState.resumed) {
+      await BackgroundLocator.unRegisterLocationUpdate();
     }
   }
 }
--- test/background_locator_test.dart
+++ test/background_locator_test.dart
@@ -3,14 +3,19 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('background_locator');
+  final TestDefaultBinaryMessenger testDBM =
+      TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger;
 
   setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
-    });
+    testDBM.setMockMethodCallHandler(
+      channel,
+      (MethodCall methodCall) async {
+        return '42';
+      },
+    );
   });
 
   tearDown(() {
-    channel.setMockMethodCallHandler(null);
+    testDBM.setMockMethodCallHandler(channel, null);
   });
 }
